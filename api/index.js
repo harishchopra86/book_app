b49d89a8674623864e47164fbea1f8f6936ea06e
Harish Chopra <harish@freshworks.io>
2019-07-02 20:12:21 -0700

    post updated to 3000

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/books', bookRoutes);
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 app.get("*", (req, res) => res.status(200).send({
     message: 'Welcome to Book App API.'
